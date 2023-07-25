91f039445ab3089e6c4522271d758e82005d5e39
Ahmed Al-Hulaibi <ahmed@envoy.com>
2023-07-25 10:22:33 -0400

    fix: make srun safer for commands that need branch context

--- git-srun.sh
+++ git-srun.sh
@@ -2,6 +2,12 @@
 
 # get the current branch name, local only
 BRANCH=$(git current)
+CURRENT_SHA=$(git rev-parse --short HEAD)
+DETACHED=$(git branch --show-current | wc -l)
+
+if [ "$DETACHED" -eq 1 ]; then
+    BRANCH="HEAD"
+fi
 
 # echo "git-srun: running $BRANCH"
 # ignore master, main, release, develop
@@ -10,5 +16,19 @@ if [ "$BRANCH" = "HEAD" ] || [ "$BRANCH" = "master" ] || [ "$BRANCH" = "main" ] 
     exit 0
 fi
 
+# checkout the current branch
+git checkout $BRANCH
+
+# catch exit codes so that we don't exit if the command fails
+set +e
+
 # run whatever command the user passed in
 "$@"
+
+exitCode=$?
+
+# checkout the previous head
+git checkout -q $CURRENT_SHA
+
+# exit with the exit code of the command
+exit $exitCode
